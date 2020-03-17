c7d4462531efb7aafc5d755a003287291397ae9b
Haopin Wu <psistwu@outlook.com>
2020-03-17 11:25:01 +0800

    Rewrite 'droptctl.py'

--- tools/droptctl.py
+++ tools/droptctl.py
@@ -1,85 +1,73 @@
-import dropt
+"""Example DrOpt client script.
+
+This script demonstrates how one can wrap a python ML model
+training/testing script with a DrOpt client script in order to submit
+an experiment to a DrOpt server.
+
+Example:
+    Before running
+    To run the script, simply execute the following command in shell:
+
+        $ python dropt_client -t [user token] -i [server IP] -c [configuration file]
+"""
+
 import time
-import random
-import argparse as arg
-from subprocess import check_output
+import dropt.client as dropt_cli
+from argparse import ArgumentParser
+from xgb import params, run
 
-parser = arg.ArgumentParser()
-parser.add_argument("-token", "--user-api-token", help="user api token", dest="token", default="")
-parser.add_argument("-proj-name", "--project-name", help="project name", dest="name", default="")
-parser.add_argument("-ip", "--server-ip", help="server ip", dest="ip", default="")
-parser.add_argument("-trial", "--trial-num", help="trail num", dest="trial", default=10)
-args = parser.parse_args()
 
-# Define API token for authorization
-conn = dropt.Connection(client_token=args.token, server_ip=args.ip)
+def header_footer_loop(func):
+    '''Decorator adding header, footer and loop.'''
+    def inner(pid, n_trial):
+        print( "\n=================== Trial Start ====================")
+        print(f"        Project ID: {pid}, Number of trials: {n_trial}")
+        print( "----------------------------------------------------")
+        for i in range(n_trial):
+            print(f"\n[trial {i+1}]")
+            func(pid, n_trial)
+        print("\n=================== Trial End ======================\n")
+    return inner
+    
 
-# Create DrOpt project
+@header_footer_loop
+def param_search(pid, n_trial):
+    '''Parameter search.'''
+    # wait for back-end processing
+    time.sleep(2)
+
+    # request hyper-parameters from DrOpt
+    sugt = conn.projects(pid).suggestions().create()
+    sugt_id = sugt.suggest_id
+    sugt_value = sugt.assignments
+
+    # apply the suggested parameters and evalute the corresponding model
+    params.update(sugt_value)
+    metric = run(params)
+    print(f"Suggestion = {sugt_value}")
+    print(f"Evaluation: {metric}")
+
+    # report result to DrOpt
+    conn.projects(pid).validations().create(
+        suggest_id = sugt_id,
+        value = metric
+    )
+
+
+# parse input arguments
+parser = ArgumentParser()
+parser.add_argument("-t", "--user-token", help="user token", required=True)
+parser.add_argument("-i", "--server-ip", help="server IP", default="52.175.54.152")
+parser.add_argument("-c", "--config", help="configuration file", default="config.json")
+args, _ = parser.parse_known_args()
+
+# establish connection to a DrOpt server using the given user token
+conn = dropt_cli.Connection(client_token=args.user_token, server_ip=args.server_ip)
+
+# create a DrOpt project
 project = conn.projects().create(
-   # Define project name
-   name=args.name,
-   # Define which parameters you would like to tune
-   # type: int - means integer parameter
-   # type: float - means float parameter
-   # type: choice - means categorical parameter
-   parameters=[
-     {'name': 'max_depth', 'type': 'int', 'min': 1,  'max': 5},
-     {'name': 'gamma', 'type': 'float', 'min': 0.1,  'max': 1.0},
-     {'name': 'subsample', 'type': 'float', 'min': 0.1, 'max': 1.0},
-     {'name': 'colsample', 'type': 'float', 'min': 0.1, 'max': 1.0},
-     {'name': 'alpha', 'type': 'float', 'min': 0.1, 'max': 1.0},
-     {'name': 'learn_rate', 'type': 'float', 'min': 0.1, 'max': 1.0}
-     # {'name': 'tcrang', 'type': 'choice', 'value': 'aaa,bbb,ccc,ddd,eee'},
-   ],
-   # tuner='auto', # or 'TPE,'
-   # Define the validation times for your project (optimization loops)
-   trial=args.trial,
+   config = dropt_cli.load_config_file(args.config)
 )
 
-# Get project id that returned from DrOpt
-project_id = project.project_id
-project_trial = project.trial
-print ("\n=================== Trial Start ====================")
-# print ("----------------------------------------------------")
-print ("        Project ID: %s, Project Trial: %s" % (project_id, project_trial))
-print ("----------------------------------------------------")
-
-# Delay wait for backend initialing
-time.sleep(1)
-
-def train(params):
-    out = check_output(["python3", "train.py",
-    "-d", params["max_depth"],
-    "-g", params["gamma"],
-    "-s", params["subsample"],
-    "-c", params["colsample"],
-    "-a", params["alpha"],
-    "-l", params["learn_rate"]
-    ])
-    
-    # Or return random number for sample (delete the check_ouput)
-    # best_acc = random.uniform(0.5, 0.9)
-    # return best_acc
-    
-    return str(out, encoding = "utf-8")[0:5]
-
-
-# Run the optimization loop
-for i in range(project_trial):
-   # Retrieve hyperparamaters from DrOpt
-   suggestion = conn.projects(project_id).suggestions().create()
-   sugt_id = suggestion.suggest_id
-   sugts = suggestion.assignments
-
-   metric = train(params=sugts)
-   print('\n[trial %d] Evaluation: %s' % (i+1, metric))
-   print("suggestion="+str(sugts))
-
-   # report to DrOpt
-   conn.projects(project_id).validations().create(
-     suggest_id = sugt_id,
-     value = float(metric),
-   )
-
-   # Delay wait for backend processing
-   time.sleep(1)
+# perform parameter search
+param_search(pid=project.project_id, n_trial=project.trial)
